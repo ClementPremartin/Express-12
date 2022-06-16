9f3212351802506a306c6a967ef82dca07edc158
ClementPremartin <clementpremartin@gmail.com>
2022-06-16 18:30:50 +0200

    init Express 12 quest

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const authRouter = require('express').Router();
+const User = require('../models/user');
+const {calculateToken} = require('../helpers/users');
+
+authRouter.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  User.findByEmail(email).then((user) => {
+    if (!user) res.status(401).send('Invalid credentials');
+    else {
+      User.verifyPassword(password, user.hashedPassword).then(
+        (passwordIsCorrect) => {
+          if (passwordIsCorrect) {
+            const token = calculateToken(email);
+            User.update(user.id, {token: token})
+            res.cookie('user_token', token)
+            res.send("You are logged in")
+          }
+          else res.status(401).send('Invalid credentials');
+        }
+      );
+    }
+  });
+});
+
+module.exports = authRouter;
